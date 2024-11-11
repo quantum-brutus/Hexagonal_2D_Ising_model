3f380cb948ea40a3bb82808a91405846d0857d58
quantum-brutus <paul.rousset-rouard@student-cs.fr>
2024-11-11 20:32:38 +0100

    hysteresis added

--- results.py
+++ results.py
@@ -10,112 +10,196 @@ import numpy as np
 
 n = [6]
 N = [n**2 for n in n]
-nb_iterations = 25000
+nb_iterations = 10
 
 
 import time
 
 start = time.time()
 
-hexagonal = True
+hexagonal = False
 plot = False  ## if true : plot the mean energies, magnetization, heat capacity and their values depending on the simulation step
             ## else : plot the mean energies, magnetization, heat capacity depending on the Temperature
 
-antiferromagnetic = True 
-
-B_star_norm = 0 # norm of the applied magnetic field, must be POSITIVE
-
-if antiferromagnetic :  
-    T = [0.1*i for i in range(1,45)]  # range of simulation temperatures. A negative value of temperature means a negative coupling coefficient between spins
-else :
-    T = [-4.5+0.1*i for i in range(0,44, 15)]  # range of simulation temperatures. A negative value of temperature means a negative coupling coefficient between spins
-
-#############################################################
+antiferromagnetic = False 
+hysteresis = True
 
 
-energies, capacities, magnetizations = [[] for i in n], [[] for i in n], [[] for i in n]
+if not(hysteresis) : 
 
-for index, number_of_atoms in enumerate(n) : 
+    B_star_norm = 0 # norm of the applied magnetic field, must be POSITIVE
 
-    for T_star in T : 
+    if antiferromagnetic :  
+        
+        T = [0.01*i for i in range(1,50)]  # range of simulation temperatures. A negative value of temperature means a negative coupling coefficient between spins
 
-        energy, capacity, magnetization = simulation(B_star_norm, T_star, number_of_atoms, nb_iterations, plot, hexagonal)
+        # T = [0.1*i for i in range(1,45)]  # range of simulation temperatures. A negative value of temperature means a negative coupling coefficient between spins
+    else :
+        T = [-4.5+0.1*i for i in range(0,44, 15)]  # range of simulation temperatures. A negative value of temperature means a negative coupling coefficient between spins
 
-        energies[index].append(energy)
-        capacities[index].append(capacity)
-        magnetizations[index].append(magnetization)
+    #############################################################
+
+    energies, capacities, magnetizations = [[] for i in n], [[] for i in n], [[] for i in n]
+
+    for index, number_of_atoms in enumerate(n) : 
+
+        for T_star in T : 
+
+            energy, capacity, magnetization = simulation(B_star_norm, T_star, number_of_atoms, nb_iterations, plot, hexagonal)
+
+            energies[index].append(energy)
+            capacities[index].append(capacity)
+            magnetizations[index].append(magnetization)
 
 
 
-if not(plot) : 
-    ### plots ###
+    if not(plot) : 
+        ### plots ###
 
-    # Create a figure with 1 row and 3 columns of subplots
-    fig, axs = plt.subplots(1, 3, figsize=(18, 5))
+        # Create a figure with 1 row and 3 columns of subplots
+        fig, axs = plt.subplots(1, 3, figsize=(18, 5))
 
-    # Define colors and styles for each plot
-    colors = ["royalblue", "darkorange", "seagreen"]
-    linestyles = ["--", "--", "--"]
-    linewidth = 1.5
+        # Define colors and styles for each plot
+        colors = ["royalblue", "darkorange", "seagreen"]
+        linestyles = ["--", "--", "--"]
+        linewidth = 1.5
+        
+        if T[0] <0 : 
+            T_corrected = [-1*i for i in T]
+
+        else : 
+            T_corrected = [i for i in T]
+
+        # Loop over each index and value of n to plot data for each n
+        for idx, n_values in enumerate(n):
+            color = colors[idx % len(colors)]
+            linestyle = linestyles[idx % len(linestyles)]
+            
+            # Subplot 1: Evolution of Average Energy for each n
+            axs[0].plot(T_corrected, energies[idx], '.', label=f'n={n_values}', linestyle=linestyle, color=color, linewidth=linewidth)
+            
+            # Subplot 2: Average Magnetization for each n
+            axs[1].plot(T_corrected, np.abs(magnetizations[idx]), '.',label=f'n={n_values}', linestyle=linestyle, color=color, linewidth=linewidth)
+            
+            # Subplot 3: Specific Heat Capacity for each n
+            axs[2].plot(T_corrected, capacities[idx], '.',label=f'n={n_values}', linestyle=linestyle, color=color, linewidth=linewidth)
+
+        # Subplot settings for titles, labels, legends, and grids
+        axs[0].set_title("Évolution de l'énergie moyenne", fontsize=14, fontweight='bold')
+        axs[0].set_xlabel("Température adimensionnée", fontsize=12)
+        axs[0].set_ylabel("Énergie adimensionnée", fontsize=12)
+        axs[0].legend(loc="best", fontsize=7)
+        axs[0].grid(True, linestyle=":", color="grey", alpha=0.6)
+
+        axs[1].set_title("Évolution de l'aimantation moyenne corrigée", fontsize=12, fontweight='bold')
+        axs[1].set_xlabel("Température adimensionnée", fontsize=12)
+        axs[1].set_ylabel("Aimantation (somme alternée en valeur absolue) adimensionnée", fontsize=6)
+        axs[1].set_ylim(0, 1)
+        axs[1].legend(loc="best", fontsize=7)
+        axs[1].grid(True, linestyle=":", color="grey", alpha=0.6)
+
+        axs[2].set_title("Capacité thermique", fontsize=14, fontweight='bold')
+        axs[2].set_xlabel("Température adimensionnée", fontsize=12)
+        axs[2].set_ylabel("Capacité thermique adimensionnée", fontsize=11)
+        axs[2].legend(loc="best", fontsize=7)
+        axs[2].grid(True, linestyle=":", color="grey", alpha=0.6)
+
+        # Adjust layout and add a main title for the figure
+        fig.suptitle("Analyse Thermodynamique", fontsize=18, fontweight='bold')
+
+
+        # Subtitle positioned slightly below the main title
+        # plt.text(0.5, 0.9, "Cas ferromagnétique : J<0 ", 
+        #         ha='center', va='top', fontsize=14, transform=fig.transFigure)
+        
+        # Adjust layout to prevent overlap
+        plt.tight_layout(pad=3)   
+
+
+
+        end = time.time()
+
+        time = end-start 
+
+        print("TIME WAS ", time)
+
+        # Show the figure
+        plt.show()
+
+
+else : ##we look at the hysteresis cycle
+
+    T = 2
     
-    if T[0] <0 : 
-        T_corrected = [-1*i for i in T]
+    ##initial state
 
-    else : 
-        T_corrected = [i for i in T]
+    initial_state_matrix = np.random.choice([-1, 1], size=(n[0], n[0]))
 
-    # Loop over each index and value of n to plot data for each n
-    for idx, n_values in enumerate(n):
-        color = colors[idx % len(colors)]
-        linestyle = linestyles[idx % len(linestyles)]
-        
-        # Subplot 1: Evolution of Average Energy for each n
-        axs[0].plot(T_corrected, energies[idx], '.', label=f'n={n_values}', linestyle=linestyle, color=color, linewidth=linewidth)
-        
-        # Subplot 2: Average Magnetization for each n
-        axs[1].plot(T_corrected, np.abs(magnetizations[idx]), '.',label=f'n={n_values}', linestyle=linestyle, color=color, linewidth=linewidth)
-        
-        # Subplot 3: Specific Heat Capacity for each n
-        axs[2].plot(T_corrected, capacities[idx], '.',label=f'n={n_values}', linestyle=linestyle, color=color, linewidth=linewidth)
+    number_of_atoms = n[0]
 
-    # Subplot settings for titles, labels, legends, and grids
-    axs[0].set_title("Évolution de l'énergie moyenne", fontsize=14, fontweight='bold')
-    axs[0].set_xlabel("Température adimensionnée", fontsize=12)
-    axs[0].set_ylabel("Énergie adimensionnée", fontsize=12)
-    axs[0].legend(loc="best", fontsize=7)
-    axs[0].grid(True, linestyle=":", color="grey", alpha=0.6)
+    energies1, capacities1, magnetizations1 = np.array([]), np.array([]), np.array([])
 
-    axs[1].set_title("Évolution de l'aimantation moyenne", fontsize=14, fontweight='bold')
-    axs[1].set_xlabel("Température adimensionnée", fontsize=12)
-    axs[1].set_ylabel("Aimantation (en valeur absolue) adimensionnée", fontsize=10)
-    axs[1].set_ylim(0, 1)
-    axs[1].legend(loc="best", fontsize=7)
-    axs[1].grid(True, linestyle=":", color="grey", alpha=0.6)
+    # B = 0 
+    # energy, capacity, magnetization, state = simulation(B, T, number_of_atoms, nb_iterations, plot, hexagonal, initial_state_matrix)
 
-    axs[2].set_title("Capacité thermique", fontsize=14, fontweight='bold')
-    axs[2].set_xlabel("Température adimensionnée", fontsize=12)
-    axs[2].set_ylabel("Capacité thermique adimensionnée", fontsize=11)
-    axs[2].legend(loc="best", fontsize=7)
-    axs[2].grid(True, linestyle=":", color="grey", alpha=0.6)
+    # np.append(energies1,energy)
+    # np.append(capacities1,capacity)
+    # np.append(magnetizations1, magnetization)
 
-    # Adjust layout and add a main title for the figure
-    fig.suptitle("Analyse Thermodynamique", fontsize=18, fontweight='bold')
+    #state is the state of the system at T=2. We increase the norm of the magnetic field.
 
+    ##first magnetization
 
-    # Subtitle positioned slightly below the main title
-    # plt.text(0.5, 0.9, "Cas ferromagnétique : J<0 ", 
-    #         ha='center', va='top', fontsize=14, transform=fig.transFigure)
+    B_values = [0.3*i for i in range(49)] + [14.7-0.3*i for i in range(100)] + [0.3*i for i in range(49)]
+
+    for B in B_values : 
+
+        energy, capacity, magnetization, state = simulation(B, T, number_of_atoms, nb_iterations, plot, hexagonal, initial_state_matrix)
     
-    # Adjust layout to prevent overlap
-    plt.tight_layout(pad=3)   
+        # Append results to arrays
+        energies1 = np.append(energies1, energy)
+        capacities1 = np.append(capacities1, capacity)
+        magnetizations1 = np.append(magnetizations1, magnetization)
+        initial_state_matrix = state  # Update the initial state to continue from the last state
 
 
+    if not(plot): 
+        ### plots ###
 
-    end = time.time()
+        # Create a figure with 1 subplot
+        fig, axs = plt.subplots(1, 2, figsize=(8, 6))  # Adjust size if needed
 
-    time = end-start 
+        # Define colors and styles for each plot
+        colors = ["royalblue"]
+        linestyles = ["--"]
+        linewidth = 1.5
+        
+        # Loop over each index and value of n to plot data for each n
+        for idx, n_values in enumerate(n):
+            color = colors[idx % len(colors)]
+            linestyle = linestyles[idx % len(linestyles)]
+            
+            # Plot the magnetization against B_values
+            axs[0].plot(B_values, magnetizations1, '.', label=f'n={number_of_atoms}', color=color, linewidth=linewidth)
+            axs[1].plot(B_values, energies1, '.', label=f'n={number_of_atoms}', color=color, linewidth=linewidth)
 
-    print("TIME WAS ", time)
+            
+        # Set titles, labels, legend, and grid
+        axs[0].set_title("Évolution de l'aimantation moyenne", fontsize=14, fontweight='bold')
+        axs[0].set_xlabel("Norme du champ magnétique adimensionnée", fontsize=12)
+        axs[0].set_ylabel("Aimantation moyenne adimensionnée", fontsize=12)
+        axs[0].legend(loc="best", fontsize=7)
+        axs[0].grid(True, linestyle=":", color="grey", alpha=0.6)
 
-    # Show the figure
-    plt.show()
+        # Main title for the figure
+        fig.suptitle("Analyse du cycle d'hystérésis", fontsize=18, fontweight='bold')
+
+        # Adjust layout to prevent overlap
+        plt.tight_layout()
+
+        end = time.time()
+        elapsed_time = end - start
+        print("TIME WAS ", elapsed_time)
+
+        # Show the figure
+        plt.show()
